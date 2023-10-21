f6786680f6132e40e137cad94bdc8750af7bf5fe
Ian-Ott <101023990+Ian-Ott@users.noreply.github.com>
2023-10-20 21:25:44 -0300

    main para ejecutar la ventana de prueba (provisional)

--- ar/edu/unlu/poo/app/NuevoJuego.java
+++ ar/edu/unlu/poo/app/NuevoJuego.java
@@ -1,4 +1,16 @@
 package ar.edu.unlu.poo.app;
 
+import ar.edu.unlu.poo.ventana.Ventana;
+
+import javax.swing.*;
+
 public class NuevoJuego {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Ventana v = new Ventana();
+            }
+        });
+    }
 }
