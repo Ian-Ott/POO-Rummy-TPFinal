49a08ba5f11e3f9fc1fdf0b55a2e1e33dc558e2d
Ian-Ott <101023990+Ian-Ott@users.noreply.github.com>
2023-11-09 16:50:44 -0300

    agregada clase jugada por lo que cambio un poco el comportamiento del juego

---
+++ ar/edu/unlu/poo/modelo/Jugada.java
@@ -0,0 +1,24 @@
+package ar.edu.unlu.poo.modelo;
+
+import java.util.ArrayList;
+
+public class Jugada {
+    private ArrayList<Carta> cartasJugada;
+    private String nombreCreadorJugada;
+
+    public void agregarCartaAJugada(Carta nuevaCarta) {
+        this.cartasJugada.add(nuevaCarta);
+    }
+
+    public ArrayList<Carta> getCartasJugada() {
+        return cartasJugada;
+    }
+
+    public void setNombreCreadorJugada(String nombreCreadorJugada) {
+        this.nombreCreadorJugada = nombreCreadorJugada;
+    }
+
+    public String getNombreCreadorJugada() {
+        return nombreCreadorJugada;
+    }
+}
